5b60e3194d3411478c85f9c250a92f797dcd1e64
WhitneyWassenaar <whitney-wassenaar@live.nl>
2026-05-09 22:25:29 +0200

    Add main with articles

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,48 @@ import './App.css'
 
 function App() {
   return (
-      <h1>Handbags & Purses</h1>
+      <>
+        <h1>Handbags & Purses</h1>
+
+        <main>
+          <article>
+            <span>
+              <img src="" alt="image"/>
+              <p></p>
+              <h4></h4>
+            </span>
+          </article>
+
+          <article>
+            <span>
+              <img src="" alt="imag"/>
+              <p></p>
+              <h4></h4>
+            </span>
+          </article>
+
+          <article>
+            <span>
+              <img src="" alt="image"/>
+              <p></p>
+              <h4></h4>
+            </span>
+          </article>
+
+          <article>
+            <span>
+              <img src="" alt="image"/>
+              <p></p>
+              <h4></h4>
+            </span>
+          </article>
+
+        </main>
+  </>
+
+
+
+
   )
 }
 
