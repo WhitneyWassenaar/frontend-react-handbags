920d65974c05a307f59e017b9f0f955e197a9709
WhitneyWassenaar <whitney-wassenaar@live.nl>
2026-05-09 20:26:44 +0200

    Add buttons to nav and click handler helper

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import './App.css'
+import logMenuButtonText from "./assets/Helpers/logMenuButtonText.js";
 
 function App() {
-  return (
-      <h1>Handbags & Purses</h1>
-  )
+    return (
+        <>
+            <h1>Handbags & Purses</h1>
+            <nav>
+                <button type="button" onClick={() => logMenuButtonText("to the collection")}>to the collection</button>
+                <button type="button" onClick={() => logMenuButtonText("shop all bags")}>shop all bags</button>
+                <button type="button" onClick={() => logMenuButtonText("pre-orders")} disabled>pre-orders</button>
+            </nav>
+        </>
+    )
 }
 
 export default App
+
+// props: alleen in React components, niet in helpers
+// parameters: overal in JavaScript
---
+++ src/assets/helpers/logMenuButtonText.js
@@ -0,0 +1,6 @@
+logMenuButtonText.js
+function logMenuButtonText(text){
+    return console.log(text);
+}
+
+export default logMenuButtonText;
